15f2deb4e22c3f0b9907c65cde80b0f0524c128e
dongzhao <dongzhao@multicorewareinc.com>
2019-09-18 10:29:23 +0800

    Add test cases for delay object(tensor, object array)

--- test_conformance/test_graph_delay.c
+++ test_conformance/test_graph_delay.c
@@ -16,6 +16,7 @@
  */
 
 #include "test_engine/test.h"
+#include "test_tensor_util.h"
 #include <VX/vx.h>
 #include <VX/vxu.h>
 
@@ -396,9 +397,403 @@ TEST(GraphDelay, testRegisterAutoAging)
     ASSERT(delay == 0);
 }
 
+typedef struct
+{
+    const char *name;
+    enum TestTensorDF fmt;
+} test_tensor_op_arg;
+
+TEST_WITH_ARG(GraphDelay, testTensor, test_tensor_op_arg,
+    ARG("Q78_DELAYTENSOR", TT_Q78),
+    ARG("U8_DELAYTENSOR", TT_U8),
+    ARG("S8_DELAYTENSOR", TT_S8))
+{
+    vx_context context = context_->vx_context_;
+    const enum TestTensorDF fmt = arg_->fmt;
+    vx_size max_dims = 0;
+    vx_enum data_type;
+    vx_uint8 fixed_point_position;
+    vx_size sizeof_data_type;
+    vx_delay delay;
+    vx_status status;
+    vx_size delaytensor_dims;
+    vx_enum delaytensor_datatype;
+    vx_uint8 delaytensor_fppos;
+    vx_size delaytensor_in0dims[MAX_TENSOR_DIMS];
+
+    ownUnpackFormat(fmt, &data_type, &fixed_point_position, &sizeof_data_type);
+
+    VX_CALL(vxQueryContext(context, VX_CONTEXT_MAX_TENSOR_DIMS, &max_dims, sizeof(max_dims)));
+    ASSERT(max_dims > 3);
+
+    size_t * const in0_dims = malloc(sizeof(*in0_dims) * max_dims);
+    ASSERT(in0_dims);
+
+    uint64_t rng;
+    {   // TODO: ownTestGetRNG() ?
+        uint64_t * seed = &CT()->seed_;
+        ASSERT(!!seed);
+        CT_RNG_INIT(rng, *seed);
+    }
+
+    for (vx_size i = 0; i < max_dims; ++i)
+    {
+        const size_t new_dim = (size_t)CT_RNG_NEXT_INT(rng, TEST_TENSOR_MIN_DIM_SZ, TEST_TENSOR_MAX_DIM_SZ + 1);
+
+        const int mask0 = !!CT_RNG_NEXT_INT(rng, 0, TEST_TENSOR_INVERSE_MASK_PROBABILITY);
+        const int mask1 = !!CT_RNG_NEXT_INT(rng, 0, TEST_TENSOR_INVERSE_MASK_PROBABILITY);
+
+        // Note: Broadcasting is described as for each dim, either in0 and in1 have the same
+        // size or "1" for a broadcasted value. And the output is strictly determined by them
+        // so that the implementation is required to support
+        // { in0, in1, out } = { 1, 5, 5 } but not { in0, in1, out } = { 1, 1, 5 }
+        // even though the KHR sample implementation currently supports both.
+        in0_dims[i] = mask0 ? new_dim : 1;
+    }
+
+    vx_tensor tensor = vxCreateTensor(context, max_dims, in0_dims, data_type, fixed_point_position);
+    ASSERT_VX_OBJECT(tensor, VX_TYPE_TENSOR);
+
+    ASSERT_VX_OBJECT(delay = vxCreateDelay(context, (vx_reference)tensor, 1), VX_TYPE_DELAY);
+
+    vx_tensor delaytensor;
+    ASSERT_VX_OBJECT(delaytensor = (vx_tensor)vxGetReferenceFromDelay(delay, 0), VX_TYPE_TENSOR);
+
+    status = vxQueryTensor(delaytensor, VX_TENSOR_NUMBER_OF_DIMS, (void *)&delaytensor_dims, sizeof(delaytensor_dims));
+    EXPECT_EQ_VX_STATUS(VX_SUCCESS, status);
+    EXPECT_EQ_INT(max_dims, delaytensor_dims);
+
+    status = vxQueryTensor(delaytensor, VX_TENSOR_DATA_TYPE, (void *)&delaytensor_datatype, sizeof(delaytensor_datatype));
+    EXPECT_EQ_VX_STATUS(VX_SUCCESS, status);
+    EXPECT_EQ_INT(data_type, delaytensor_datatype);
+
+    status = vxQueryTensor(delaytensor, VX_TENSOR_FIXED_POINT_POSITION, (void *)&delaytensor_fppos, sizeof(delaytensor_fppos));
+    EXPECT_EQ_VX_STATUS(VX_SUCCESS, status);
+    EXPECT_EQ_INT(max_dims, delaytensor_dims);
+
+    status = vxQueryTensor(delaytensor, VX_TENSOR_DIMS, (void *)&delaytensor_in0dims, sizeof(vx_size)*max_dims);
+    EXPECT_EQ_VX_STATUS(VX_SUCCESS, status);
+
+    for (vx_size i = 0; i < max_dims; i++)
+    {
+        EXPECT_EQ_INT(in0_dims[i], delaytensor_in0dims[i]);
+    }
+
+	VX_CALL(vxReleaseDelay(&delay));
+	VX_CALL(vxReleaseTensor(&tensor));
+
+	ASSERT(delay == 0);
+	ASSERT(tensor == 0);
+
+    free(in0_dims);
+}
+
+typedef struct
+{
+    const char* testName;
+    const char* p;
+    vx_enum item_type;
+} Obj_Array_Arg;
+
+static vx_reference own_create_exemplar(vx_context context, vx_enum item_type)
+{
+    vx_reference exemplar = NULL;
+
+    vx_uint8 value = 0;
+    vx_enum format = VX_DF_IMAGE_U8;
+    vx_uint32 obj_width = 128, obj_height = 128;
+    vx_enum obj_item_type = VX_TYPE_UINT8;
+    vx_size capacity = 100;
+    vx_size levels = 8;
+    vx_float32 scale = 0.5f;
+    vx_size bins = 36;
+    vx_int32 offset = 0;
+    vx_uint32 range = 360;
+    vx_enum thresh_type = VX_THRESHOLD_TYPE_BINARY;
+    vx_size lut_num_items = 100;
+    vx_size m = 5, n = 5;
+
+    switch (item_type)
+    {
+    case VX_TYPE_IMAGE:
+        exemplar = (vx_reference)vxCreateImage(context, obj_width, obj_height, format);
+        break;
+    case VX_TYPE_ARRAY:
+        exemplar = (vx_reference)vxCreateArray(context, obj_item_type, capacity);
+        break;
+    case VX_TYPE_PYRAMID:
+        exemplar = (vx_reference)vxCreatePyramid(context, levels, scale, obj_width, obj_height, format);
+        break;
+    case VX_TYPE_SCALAR:
+        exemplar = (vx_reference)vxCreateScalar(context, obj_item_type, &value);
+        break;
+    case VX_TYPE_MATRIX:
+        exemplar = (vx_reference)vxCreateMatrix(context, obj_item_type, m, n);
+        break;
+    case VX_TYPE_DISTRIBUTION:
+        exemplar = (vx_reference)vxCreateDistribution(context, bins, offset, range);
+        break;
+    case VX_TYPE_REMAP:
+        exemplar = (vx_reference)vxCreateRemap(context, obj_width, obj_height, obj_width, obj_height);
+        break;
+    case VX_TYPE_LUT:
+        exemplar = (vx_reference)vxCreateLUT(context, obj_item_type, lut_num_items);
+        break;
+    case VX_TYPE_THRESHOLD:
+        exemplar = (vx_reference)vxCreateThreshold(context, thresh_type, obj_item_type);
+        break;
+    default:
+        break;
+    }
+
+    return exemplar;
+}
+
+static void own_check_meta(vx_reference item, vx_reference ref)
+{
+    vx_enum ref_type, item_type;
+
+    VX_CALL(vxQueryReference(ref, VX_REFERENCE_TYPE, &ref_type, sizeof(ref_type)));
+
+    VX_CALL(vxQueryReference(item, VX_REFERENCE_TYPE, &item_type, sizeof(item_type)));
+
+    ASSERT(item_type == ref_type);
+
+    switch (item_type)
+    {
+    case VX_TYPE_IMAGE:
+    {
+        vx_uint32 ref_width, item_width;
+        vx_uint32 ref_height, item_height;
+        vx_df_image ref_format, item_format;
+
+        VX_CALL(vxQueryImage((vx_image)ref, VX_IMAGE_WIDTH, &ref_width, sizeof(ref_width)));
+        VX_CALL(vxQueryImage((vx_image)ref, VX_IMAGE_HEIGHT, &ref_height, sizeof(ref_height)));
+        VX_CALL(vxQueryImage((vx_image)ref, VX_IMAGE_FORMAT, &ref_format, sizeof(ref_format)));
+
+        VX_CALL(vxQueryImage((vx_image)item, VX_IMAGE_WIDTH, &item_width, sizeof(item_width)));
+        VX_CALL(vxQueryImage((vx_image)item, VX_IMAGE_HEIGHT, &item_height, sizeof(item_height)));
+        VX_CALL(vxQueryImage((vx_image)item, VX_IMAGE_FORMAT, &item_format, sizeof(item_format)));
+
+        ASSERT(ref_width == item_width);
+        ASSERT(ref_height == item_height);
+        ASSERT(ref_format == item_format);
+    }   break;
+    case VX_TYPE_ARRAY:
+    {
+        vx_size ref_capacity, item_capacity;
+        vx_enum ref_itemtype, item_itemtype;
+
+        VX_CALL(vxQueryArray((vx_array)ref, VX_ARRAY_CAPACITY, &ref_capacity, sizeof(ref_capacity)));
+        VX_CALL(vxQueryArray((vx_array)ref, VX_ARRAY_ITEMTYPE, &ref_itemtype, sizeof(ref_itemtype)));
+
+        VX_CALL(vxQueryArray((vx_array)item, VX_ARRAY_CAPACITY, &item_capacity, sizeof(item_capacity)));
+        VX_CALL(vxQueryArray((vx_array)item, VX_ARRAY_ITEMTYPE, &item_itemtype, sizeof(item_itemtype)));
+
+        ASSERT(ref_capacity == item_capacity);
+        ASSERT(ref_itemtype == item_itemtype);
+    }   break;
+    case VX_TYPE_PYRAMID:
+    {
+        vx_uint32 ref_width, item_width;
+        vx_uint32 ref_height, item_height;
+        vx_df_image ref_format, item_format;
+        vx_size ref_levels, item_levels;
+        vx_float32 ref_scale, item_scale;
+
+        VX_CALL(vxQueryPyramid((vx_pyramid)ref, VX_PYRAMID_WIDTH, &ref_width, sizeof(ref_width)));
+        VX_CALL(vxQueryPyramid((vx_pyramid)ref, VX_PYRAMID_HEIGHT, &ref_height, sizeof(ref_height)));
+        VX_CALL(vxQueryPyramid((vx_pyramid)ref, VX_PYRAMID_FORMAT, &ref_format, sizeof(ref_format)));
+        VX_CALL(vxQueryPyramid((vx_pyramid)ref, VX_PYRAMID_LEVELS, &ref_levels, sizeof(ref_levels)));
+        VX_CALL(vxQueryPyramid((vx_pyramid)ref, VX_PYRAMID_SCALE, &ref_scale, sizeof(ref_scale)));
+
+        VX_CALL(vxQueryPyramid((vx_pyramid)item, VX_PYRAMID_WIDTH, &item_width, sizeof(item_width)));
+        VX_CALL(vxQueryPyramid((vx_pyramid)item, VX_PYRAMID_HEIGHT, &item_height, sizeof(item_height)));
+        VX_CALL(vxQueryPyramid((vx_pyramid)item, VX_PYRAMID_FORMAT, &item_format, sizeof(item_format)));
+        VX_CALL(vxQueryPyramid((vx_pyramid)item, VX_PYRAMID_LEVELS, &item_levels, sizeof(item_levels)));
+        VX_CALL(vxQueryPyramid((vx_pyramid)item, VX_PYRAMID_SCALE, &item_scale, sizeof(item_scale)));
+
+        ASSERT(ref_width == item_width);
+        ASSERT(ref_height == item_height);
+        ASSERT(ref_format == item_format);
+        ASSERT(ref_levels == item_levels);
+        ASSERT(ref_scale == item_scale);
+    }   break;
+    case VX_TYPE_SCALAR:
+    {
+        vx_enum ref_type, item_type;
+
+        VX_CALL(vxQueryScalar((vx_scalar)ref, VX_SCALAR_TYPE, &ref_type, sizeof(ref_type)));
+
+        VX_CALL(vxQueryScalar((vx_scalar)item, VX_SCALAR_TYPE, &item_type, sizeof(item_type)));
+
+        ASSERT(ref_type == item_type);
+    }   break;
+    case VX_TYPE_MATRIX:
+    {
+        vx_enum ref_type, item_type;
+        vx_size ref_rows, item_rows;
+        vx_size ref_cols, item_cols;
+
+        VX_CALL(vxQueryMatrix((vx_matrix)ref, VX_MATRIX_TYPE, &ref_type, sizeof(ref_type)));
+        VX_CALL(vxQueryMatrix((vx_matrix)ref, VX_MATRIX_ROWS, &ref_rows, sizeof(ref_rows)));
+        VX_CALL(vxQueryMatrix((vx_matrix)ref, VX_MATRIX_COLUMNS, &ref_cols, sizeof(ref_cols)));
+
+        VX_CALL(vxQueryMatrix((vx_matrix)item, VX_MATRIX_TYPE, &item_type, sizeof(item_type)));
+        VX_CALL(vxQueryMatrix((vx_matrix)item, VX_MATRIX_ROWS, &item_rows, sizeof(item_rows)));
+        VX_CALL(vxQueryMatrix((vx_matrix)item, VX_MATRIX_COLUMNS, &item_cols, sizeof(item_cols)));
+
+        ASSERT(ref_type == item_type);
+        ASSERT(ref_rows == item_rows);
+        ASSERT(ref_cols == item_cols);
+    }   break;
+    case VX_TYPE_DISTRIBUTION:
+    {
+        vx_size ref_bins, item_bins;
+        vx_int32 ref_offset, item_offset;
+        vx_uint32 ref_range, item_range;
+
+        VX_CALL(vxQueryDistribution((vx_distribution)ref, VX_DISTRIBUTION_BINS, &ref_bins, sizeof(ref_bins)));
+        VX_CALL(vxQueryDistribution((vx_distribution)ref, VX_DISTRIBUTION_OFFSET, &ref_offset, sizeof(ref_offset)));
+        VX_CALL(vxQueryDistribution((vx_distribution)ref, VX_DISTRIBUTION_RANGE, &ref_range, sizeof(ref_range)));
+
+        VX_CALL(vxQueryDistribution((vx_distribution)item, VX_DISTRIBUTION_BINS, &item_bins, sizeof(item_bins)));
+        VX_CALL(vxQueryDistribution((vx_distribution)item, VX_DISTRIBUTION_OFFSET, &item_offset, sizeof(item_offset)));
+        VX_CALL(vxQueryDistribution((vx_distribution)item, VX_DISTRIBUTION_RANGE, &item_range, sizeof(item_range)));
+
+        ASSERT(ref_bins == item_bins);
+        ASSERT(ref_offset == item_offset);
+        ASSERT(ref_range == item_range);
+    }   break;
+    case VX_TYPE_REMAP:
+    {
+        vx_uint32 ref_srcwidth, item_srcwidth;
+        vx_uint32 ref_srcheight, item_srcheight;
+        vx_uint32 ref_dstwidth, item_dstwidth;
+        vx_uint32 ref_dstheight, item_dstheight;
+
+        VX_CALL(vxQueryRemap((vx_remap)ref, VX_REMAP_SOURCE_WIDTH, &ref_srcwidth, sizeof(ref_srcwidth)));
+        VX_CALL(vxQueryRemap((vx_remap)ref, VX_REMAP_SOURCE_HEIGHT, &ref_srcheight, sizeof(ref_srcheight)));
+        VX_CALL(vxQueryRemap((vx_remap)ref, VX_REMAP_DESTINATION_WIDTH, &ref_dstwidth, sizeof(ref_dstwidth)));
+        VX_CALL(vxQueryRemap((vx_remap)ref, VX_REMAP_DESTINATION_HEIGHT, &ref_dstheight, sizeof(ref_dstheight)));
+
+        VX_CALL(vxQueryRemap((vx_remap)item, VX_REMAP_SOURCE_WIDTH, &item_srcwidth, sizeof(item_srcwidth)));
+        VX_CALL(vxQueryRemap((vx_remap)item, VX_REMAP_SOURCE_HEIGHT, &item_srcheight, sizeof(item_srcheight)));
+        VX_CALL(vxQueryRemap((vx_remap)item, VX_REMAP_DESTINATION_WIDTH, &item_dstwidth, sizeof(item_dstwidth)));
+        VX_CALL(vxQueryRemap((vx_remap)item, VX_REMAP_DESTINATION_HEIGHT, &item_dstheight, sizeof(item_dstheight)));
+
+        ASSERT(ref_srcwidth == item_srcwidth);
+        ASSERT(ref_srcheight == item_srcheight);
+        ASSERT(ref_dstwidth == item_dstwidth);
+        ASSERT(ref_dstheight == item_dstheight);
+    }   break;
+    case VX_TYPE_LUT:
+    {
+        vx_enum ref_type, item_type;
+        vx_size ref_count, item_count;
+
+        VX_CALL(vxQueryLUT((vx_lut)ref, VX_LUT_TYPE, &ref_type, sizeof(ref_type)));
+        VX_CALL(vxQueryLUT((vx_lut)ref, VX_LUT_COUNT, &ref_count, sizeof(ref_count)));
+
+        VX_CALL(vxQueryLUT((vx_lut)item, VX_LUT_TYPE, &item_type, sizeof(item_type)));
+        VX_CALL(vxQueryLUT((vx_lut)item, VX_LUT_COUNT, &item_count, sizeof(item_count)));
+
+        ASSERT(ref_type == item_type);
+        ASSERT(ref_count == item_count);
+    }   break;
+    case VX_TYPE_THRESHOLD:
+    {
+        vx_enum ref_type, item_type;
+
+        VX_CALL(vxQueryThreshold((vx_threshold)ref, VX_THRESHOLD_TYPE, &ref_type, sizeof(ref_type)));
+
+        VX_CALL(vxQueryThreshold((vx_threshold)item, VX_THRESHOLD_TYPE, &item_type, sizeof(item_type)));
+
+        ASSERT(ref_type == item_type);
+    }   break;
+    default:
+        ASSERT(0 == 1);
+    }
+}
+
+#define ADD_VX_OBJECT_ARRAY_TYPES(testArgName, nextmacro, ...) \
+    CT_EXPAND(nextmacro(testArgName "/VX_TYPE_IMAGE", __VA_ARGS__, VX_TYPE_IMAGE)), \
+    CT_EXPAND(nextmacro(testArgName "/VX_TYPE_ARRAY", __VA_ARGS__, VX_TYPE_ARRAY)), \
+    CT_EXPAND(nextmacro(testArgName "/VX_TYPE_PYRAMID", __VA_ARGS__, VX_TYPE_PYRAMID)), \
+    CT_EXPAND(nextmacro(testArgName "/VX_TYPE_SCALAR", __VA_ARGS__, VX_TYPE_SCALAR)), \
+    CT_EXPAND(nextmacro(testArgName "/VX_TYPE_MATRIX", __VA_ARGS__, VX_TYPE_MATRIX)), \
+    CT_EXPAND(nextmacro(testArgName "/VX_TYPE_DISTRIBUTION", __VA_ARGS__, VX_TYPE_DISTRIBUTION)), \
+    CT_EXPAND(nextmacro(testArgName "/VX_TYPE_REMAP", __VA_ARGS__, VX_TYPE_REMAP)), \
+    CT_EXPAND(nextmacro(testArgName "/VX_TYPE_LUT", __VA_ARGS__, VX_TYPE_LUT)), \
+    CT_EXPAND(nextmacro(testArgName "/VX_TYPE_THRESHOLD", __VA_ARGS__, VX_TYPE_THRESHOLD ))
+
+#define PARAMETERS \
+    CT_GENERATE_PARAMETERS("object_array", ADD_VX_OBJECT_ARRAY_TYPES, ARG, NULL)
+
+TEST_WITH_ARG(GraphDelay, testObjectArray, Obj_Array_Arg, PARAMETERS)
+{
+    vx_context context = context_->vx_context_;
+    vx_delay delay;
+    vx_reference exemplar = NULL;
+    vx_size num_items = 1;
+    vx_enum item_type = arg_->item_type;
+
+    vx_object_array object_array = 0;
+    vx_object_array delayobjarray;
+
+    vx_reference expect_item = NULL;
+    vx_enum expect_type = VX_TYPE_INVALID;
+    vx_size expect_num_items = 0;
+
+    vx_uint32 i;
+
+    ASSERT_VX_OBJECT(exemplar = own_create_exemplar(context, item_type), (enum vx_type_e)item_type);
+
+    /* 1. check if object array can be created with allowed types*/
+    ASSERT_VX_OBJECT(object_array = vxCreateObjectArray(context, exemplar, num_items), VX_TYPE_OBJECT_ARRAY);
+
+    /* 2. create delay with object array*/
+    ASSERT_VX_OBJECT(delay = vxCreateDelay(context, (vx_reference)object_array, 1), VX_TYPE_DELAY);
+
+    ASSERT_VX_OBJECT(delayobjarray = (vx_object_array)vxGetReferenceFromDelay(delay, 0), VX_TYPE_OBJECT_ARRAY);
+
+    VX_CALL(vxQueryObjectArray(delayobjarray, VX_OBJECT_ARRAY_ITEMTYPE, &expect_type, sizeof(expect_type)));
+    ASSERT_EQ_INT(item_type, expect_type);
+
+    VX_CALL(vxQueryObjectArray(delayobjarray, VX_OBJECT_ARRAY_NUMITEMS, &expect_num_items, sizeof(expect_num_items)));
+    ASSERT_EQ_INT(num_items, expect_num_items);
+
+    for (i = 0u; i < num_items; i++)
+    {
+        ASSERT_VX_OBJECT(expect_item = vxGetObjectArrayItem(delayobjarray, i), (enum vx_type_e)item_type);
+
+        ASSERT_NO_FAILURE(own_check_meta(expect_item, exemplar));
+
+        VX_CALL(vxReleaseReference(&expect_item));
+        ASSERT(expect_item == 0);
+    }
+
+    expect_item = vxGetObjectArrayItem(delayobjarray, (vx_uint32)num_items);
+    ASSERT_NE_VX_STATUS(VX_SUCCESS, vxGetStatus((vx_reference)expect_item));
+
+    VX_CALL(vxReleaseReference(&exemplar));
+    ASSERT(exemplar == 0);
+
+    VX_CALL(vxReleaseObjectArray(&object_array));
+    ASSERT(object_array == 0);
+
+    VX_CALL(vxReleaseObjectArray(&delayobjarray));
+    ASSERT(delayobjarray == 0);
+
+    VX_CALL(vxReleaseDelay(&delay));
+    ASSERT(delay == 0);
+}
+
 TESTCASE_TESTS(
     GraphDelay,
     testSimple,
     testPyramid,
-    testRegisterAutoAging
+    testRegisterAutoAging,
+    testTensor,
+    testObjectArray
     )
